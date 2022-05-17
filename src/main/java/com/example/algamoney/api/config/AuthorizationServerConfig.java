94758e0ba20fb9c289cd82b9b0b25cf1d05b98bf
Giovanny <gmontinny@hotmail.com>
2022-05-17 19:01:14 -0400

    Alteracao para pegar dados do usuario

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,5 +1,9 @@
 package com.example.algamoney.api.config;
 
+import java.util.Arrays;
+
+import com.example.algamoney.api.config.token.CustomTokenEnhancer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +14,8 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -47,10 +53,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
 		endpoints
 			.authenticationManager(authenticationManager)
 			.userDetailsService(userDetailsService)
-			.accessTokenConverter(accessTokenConverter())
+			.tokenEnhancer(tokenEnhancerChain)
 			.tokenStore(tokenStore())
 			.reuseRefreshTokens(false);
 	}
@@ -67,6 +77,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+		return new CustomTokenEnhancer();
 	}
 
 
